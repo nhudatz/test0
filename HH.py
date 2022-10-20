f20c0409b4c1f0d3a3779ffd3111c4508f5d7f95
nhudatz <115996564+nhudatz@users.noreply.github.com>
2022-10-20 14:06:12 +0800

    Update HH.py

--- HH.py
+++ HH.py
@@ -10,3 +10,15 @@ data = pd.read_csv(r'train.csv',)
 
 if st.checkbox('Show Dataframe'):
   st.write(data)
+  
+  st.write (data.isna().sum())
+  st.write (data.columns)
+
+
+
+data = pd.data(np.random.randn(10, 5),
+  columns = ('col %d' % i
+    for i in range(5)))
+data
+st.write('This is a line_chart.')
+st.line_chart(data)
